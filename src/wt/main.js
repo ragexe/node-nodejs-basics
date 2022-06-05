7636b3e41d685c409c96357f6b1793c673ce4bff
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-06 03:47:15 +0400

    feat: implement worker-threads subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,47 @@
+import { cpus } from "os";
+import * as path from "path";
+import { Worker } from "worker_threads";
+
+const WORKER_FILE_NAME = "worker.js";
+const WORKER_FILE_PATH = path.join(
+  path.resolve(),
+  "src",
+  "wt",
+  WORKER_FILE_NAME
+);
+
+const BASIC_NUM = 10;
+
 export const performCalculations = async () => {
-    // Write your code here
+  const results = [];
+  const logicalCPUCores = cpus();
+
+  const ordinalNumbers = new Array(logicalCPUCores.length)
+    .fill()
+    .map((_, index) => index + BASIC_NUM);
+
+  await Promise.all(
+    ordinalNumbers.map(
+      (ordinalNumber, index) =>
+        new Promise((resolve, reject) => {
+          const worker = new Worker(WORKER_FILE_PATH);
+          worker.postMessage(ordinalNumber);
+
+          worker
+            .on("error", (error) => reject(error))
+            .on("message", (result) => {
+              results.push([index, result]);
+              worker.terminate().then(() => {
+                resolve();
+              });
+            });
+        })
+    )
+  );
+
+  return results
+    .sort(([leftIndex, _], [rightIndex, __]) => {
+      return leftIndex - rightIndex;
+    })
+    .map(([_, result]) => result);
 };
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,17 @@
-// n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
 
-export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+export const sendResult = (ordinalNumber) => {
+  try {
+    const nthFibonacciValue = nthFibonacci(ordinalNumber);
+    parentPort.postMessage({ status: "resolved", data: nthFibonacciValue });
+  } catch (err) {
+    parentPort.postMessage({ status: "error", data: null });
+  }
 };
+
+parentPort.on("message", (ordinalNumber) => {
+  sendResult(ordinalNumber);
+});
