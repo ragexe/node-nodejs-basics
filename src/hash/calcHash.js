b58aa739d7f082d8f7b6504ac7c942ed64fdfdc8
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-06 01:17:25 +0400

    feat: implement hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import { createHash } from "crypto";
+import { readFile } from "node:fs/promises";
+import * as path from "path";
+
+const SOURCE_FILE_NAME = "fileToCalculateHashFor.txt";
+const SOURCE_FILE_PATH = path.join(
+  path.resolve(),
+  "src",
+  "hash",
+  "files",
+  SOURCE_FILE_NAME
+);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const dataBuffer = await readFile(SOURCE_FILE_PATH, { flag: "r" });
+  const sha256Hash = createHash("sha256").update(dataBuffer);
+  const hexString = sha256Hash.digest("hex");
+
+  return hexString;
 };
