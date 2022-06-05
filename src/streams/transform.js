66247a963d5ac3300fcc423b6946fd320c984a07
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-06 02:31:58 +0400

    feat: implement stremas subtask

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,20 @@
+import { pipeline } from "node:stream/promises";
+import { Transform } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+  const writableStream = process.stdout;
+  const readableStream = process.stdin;
+
+  const reverseTransformer = new Transform({
+    transform: (chunk, _, callback) => {
+      const result = [
+        ...chunk.toString().split("").slice(0, -1).reverse(),
+        "\n",
+      ].join("");
+
+      callback(null, result);
+    },
+  });
+
+  return pipeline(readableStream, reverseTransformer, writableStream);
 };
