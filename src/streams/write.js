a0724072c9d32df4ed15845e1ce71122338ece23
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-06 01:28:16 +0400

    feat: implement stremas subtask

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,28 @@
+import { createWriteStream } from "fs";
+import * as path from "path";
+
+const SOURCE_FILE_NAME = "fileToWrite.txt";
+const SOURCE_FILE_PATH = path.join(
+  path.resolve(),
+  "src",
+  "streams",
+  "files",
+  SOURCE_FILE_NAME
+);
+
 export const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(SOURCE_FILE_PATH);
+  const promise = new Promise((resolve, reject) => {
+    process.stdin
+      .on("data", (chunk) => writableStream.write(chunk))
+      .on("end", () => writableStream.write("\n"))
+      .on("close", () => {
+        resolve(void 0);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+
+  return promise;
 };
