0ff284e4ec615328f50397b980f0e6a8a6b16093
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-06 00:00:52 +0400

    feat: implement cli subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
+const VAR_PREFIX = "RSS_";
+
 export const parseEnv = () => {
-    // Write your code here 
+  const envVariables = Object.entries((process && process.env) || {});
+  const prefixedEnvVariables = envVariables.filter(([key, _]) =>
+    key.startsWith(VAR_PREFIX)
+  );
+
+  const result = prefixedEnvVariables
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log(result);
 };
