0c782dae9be2f7492b0b2bb287d2ec426bb39b00
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-05 22:49:14 +0400

    feat: implement filesystem subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,45 @@
+import { cp, stat } from "node:fs/promises";
+import * as path from "path";
+
+const SOURCE_FOLDER_NAME = "files";
+const TARGET_FOLDER_NAME = "files_copy";
+
+const [SOURCE_FOLDER_PATH, TARGET_FOLDER_PATH] = [
+  SOURCE_FOLDER_NAME,
+  TARGET_FOLDER_NAME,
+].map((folderName) => path.join(path.resolve(), "src", "fs", folderName));
+
+class CustomError extends Error {
+  code = "SRC_NOT_A_FOLDER";
+
+  constructor(message) {
+    super(message);
+  }
+}
+
 export const copy = async () => {
-    // Write your code here 
+  try {
+    const stats = await stat(SOURCE_FOLDER_PATH);
+
+    if (!stats.isDirectory()) {
+      throw new CustomError();
+    }
+
+    await cp(SOURCE_FOLDER_PATH, TARGET_FOLDER_PATH, {
+      errorOnExist: true,
+      recursive: true,
+      force: false,
+    });
+
+    return Promise.resolve(void 0);
+  } catch (error) {
+    if (
+      error &&
+      ["ERR_FS_CP_EEXIST", "ENOENT", "SRC_NOT_A_FOLDER"].includes(error.code)
+    ) {
+      throw new Error("FS operation failed");
+    }
+
+    throw error;
+  }
 };
